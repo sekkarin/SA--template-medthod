2c0a3e316b3a1879329342f704878bb7528b208c
Sekkarin <sekkri1234@gmail.com>
2023-09-04 08:57:00 +0000

    first commit

---
+++ src/main/java/templatemethod/TemplateMethodDemo.java
@@ -0,0 +1,8 @@
+package templatemethod;
+
+public class TemplateMethodDemo {
+    public static void main(String[] args) {
+        Generalization algorithm = new Realization();
+        algorithm.findSolution();
+    }
+}
